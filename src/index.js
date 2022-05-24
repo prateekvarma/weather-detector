978196848b3be98dbb8b97ed178690b50e2b6ea0
Prateek Varma <prtk.varma@gmail.com>
2022-05-24 16:17:25 -0600

    used componentDidMount() lifecycle methodd to get API data from browser

--- src/index.js
+++ src/index.js
@@ -5,23 +5,22 @@ class App extends React.Component {
   //below, we've initialized a state without using a constructor
   state = { lat: null, errorMessage: "" };
 
-  render() {
+  componentDidMount() {
     //below we're using the browser's API and extracting info from it
     // more info : https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
     //The getCurrentPosition() function takes 2 callback arguments, first is for success & 2nd is for errors
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
+  }
+
+  render() {
     return (
-    <div>
-      Latitute: {this.state.lat} <br />
-      Error: {this.state.errorMessage}
-    </div>
+      <div>
+        Latitute: {this.state.lat} <br />
+        Error: {this.state.errorMessage}
+      </div>
     );
   }
 }
